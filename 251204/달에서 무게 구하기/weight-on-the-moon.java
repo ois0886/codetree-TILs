d46ac4ec9287e8f4180503c721cd5ccdf8dfbfe2
oh in seong <ohinsung64@gmail.com>
2025-12-04 16:51:30 +0900

    Title: 달에서 무게 구하기, Time: 99ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251204/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double b = 0.165;
+        System.out.printf("13 * 0.165000 = %.6f", (double)a*b);
+    }
+}
