36ae8197dae5c005f57d512d865b477143c2d3f6
oh in seong <ohinsung64@gmail.com>
2025-12-04 16:54:34 +0900

    Title: 점수 입출력, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251204/점수 입출력/enter-int-and-print-score.java
+++ 251204/점수 입출력/enter-int-and-print-score.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        if(1 <= input && input <= 100){
+        if(1 <= input && input <= 1000){
             System.out.println(input);
         }else{
             System.out.println("Your score is " + input + " point.");
