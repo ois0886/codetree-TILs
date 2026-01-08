9da73a93ede18c8d8c5a3ff81626f1af715ce0cd
oh in seong <ohinsung64@gmail.com>
2026-01-08 15:09:15 +0900

    Title: 특정 조건 두 정수 비교, Time: 147ms, Memory: 10MB, Status: Passed - Codetree

--- 260108/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 260108/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(Sysytem.in);
+        Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
         if (A < B){
