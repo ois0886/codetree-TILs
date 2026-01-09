710391db4cd813e15bb6ccdc75313ebf57efde3c
oh in seong <ohinsung64@gmail.com>
2026-01-09 15:15:07 +0900

    Title: 등급 매기기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260109/등급 매기기/ranking.java
+++ 260109/등급 매기기/ranking.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        if(num >= 90) Sysetm.out.println("A");
-        else if(num >= 80) Sysetm.out.println("B");
-        else if(num >= 70) Sysetm.out.println("C");
-        else if(num >= 60) Sysetm.out.println("D");
-        else Sysetm.out.println("F");
+        if(num >= 90) System.out.println("A");
+        else if(num >= 80) System.out.println("B");
+        else if(num >= 70) System.out.println("C");
+        else if(num >= 60) System.out.println("D");
+        else System.out.println("F");
     }
 }
